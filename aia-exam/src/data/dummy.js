fcd62c0320fc1d10f6e89871d6d64dacc74cdf3b
babyOctopuses <41468989+babyOctopuses@users.noreply.github.com>
2022-10-23 17:21:53 +0700

    first CRA

---
+++ aia-exam/src/data/dummy.js
@@ -0,0 +1,7 @@
+const products = [
+  { title: "Cabbage", isFruit: false, id: 1 },
+  { title: "Garlic", isFruit: false, id: 2 },
+  { title: "Apple", isFruit: true, id: 3 },
+];
+
+export default products;
